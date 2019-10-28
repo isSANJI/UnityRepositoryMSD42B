6a1e714f297390e65d353b0cd96ff7a328accb1b
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-10-28 14:21:43 +0100

    Created 2 Levels, Prefabs, Start Menu with Buttons
    Created SceneLoader methods LoadLevel() and Quit()

---
+++ BlockBreakerMSD42B/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        //Application.Quit();
+
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+       
+
+}
