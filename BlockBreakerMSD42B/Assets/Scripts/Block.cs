abd672fe6c1d34461344a7d26e5b1f2c43740ee6
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-04 14:29:23 +0100

    Load Next Level when all blocks destroyed

--- BlockBreakerMSD42B/Assets/Scripts/Block.cs
+++ BlockBreakerMSD42B/Assets/Scripts/Block.cs
@@ -6,10 +6,23 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    void Start()
+    {
+        //find object of type Level and save it in level
+        level = FindObjectOfType<Level>();
+
+        //add 1 to breakableBlocks
+        level.CountBreakableBlocks();
+
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //play the AudioClip from the Camera position
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
---
+++ BlockBreakerMSD42B/Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks;
+
+    SceneLoader sceneLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        //when blocks == 0 load next scene
+        if(breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+}
