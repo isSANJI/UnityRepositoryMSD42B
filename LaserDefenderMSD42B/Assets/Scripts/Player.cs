e57a4c93db9ed47145735372214e8ce22c9ac336
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-18 14:28:40 +0100

    Created Laser Defender; moving spaceship

---
+++ LaserDefenderMSD42B/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private float MoveX()
+    {
+        float deltaX = Input.GetAxis("Horizontal");
+        float newXPos = transform.position.x + deltaX;
+
+        return newXPos;
+    }
+
+    private float MoveY()
+    {
+        float deltaY = Input.GetAxis("Vertical");
+        float newYPos = transform.position.y + deltaY;
+
+        return newYPos;
+    }
+
+    private void Move()
+    {
+        transform.position = new Vector2(MoveX(), MoveY());
+
+        
+    }
+}
