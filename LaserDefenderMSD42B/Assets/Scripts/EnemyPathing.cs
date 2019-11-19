e71cb4c7947f8e95fdc770d84e764c4a44b8ff7b
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-19 09:55:33 +0100

    SetUp Boundaries, Created Waypoint on Path (Enemy)

---
+++ LaserDefenderMSD42B/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+
+    [SerializeField] float enemySpeed = 2f;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the Enemy position to the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- LaserDefenderMSD42B/Assets/Scripts/Player.cs
+++ LaserDefenderMSD42B/Assets/Scripts/Player.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float movementSpeed = 10f;
+
+    float xMin, xMax, yMin, yMax;
+
+    float padding = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        SetUpBoundaries();
         
     }
 
@@ -16,9 +24,20 @@ public class Player : MonoBehaviour
         Move();
     }
 
+    private void SetUpBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
     private float MoveX()
     {
-        float deltaX = Input.GetAxis("Horizontal");
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float newXPos = transform.position.x + deltaX;
 
         return newXPos;
@@ -26,7 +45,7 @@ public class Player : MonoBehaviour
 
     private float MoveY()
     {
-        float deltaY = Input.GetAxis("Vertical");
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
         float newYPos = transform.position.y + deltaY;
 
         return newYPos;
@@ -34,7 +53,11 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        transform.position = new Vector2(MoveX(), MoveY());
+        //clamping the values between xMin and xMax; yMin and yMax
+        float xPos = Mathf.Clamp(MoveX(), xMin, xMax);
+        float yPos = Mathf.Clamp(MoveY(), yMin, yMax);
+
+        transform.position = new Vector2(xPos, yPos);
 
         
     }
