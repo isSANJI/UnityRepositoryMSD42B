52322bde3a7811f7a56426aa286153eb40160bc4
Neil Aquilina <neil.aquilina@mcast.edu.mt>
2019-11-26 08:33:58 +0100

    Enemy moving on Path

--- LaserDefenderMSD42B/Assets/Scripts/EnemyPathing.cs
+++ LaserDefenderMSD42B/Assets/Scripts/EnemyPathing.cs
@@ -7,7 +7,7 @@ public class EnemyPathing : MonoBehaviour
 
     [SerializeField] List<Transform> waypoints;
 
-    [SerializeField] float enemySpeed = 2f;
+    [SerializeField] float enemySpeed = 12f;
 
     int waypointIndex = 0;
 
@@ -21,6 +21,36 @@ public class EnemyPathing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMovement();
+    }
+
+    private void EnemyMovement()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            //set the targetPosition to the position of our next wayPoint
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+
+            float movementThisFrame = enemySpeed * Time.deltaTime;
+            
+            //move the enemy from its current position towards the targetPosition
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+
+            
+
+        }
+        //if enemy moved to all waypoints
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
     }
 }
